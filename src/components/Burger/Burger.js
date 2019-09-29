65dfb5c5fd21dccd2975dba085f6d9271c10fae1
Vu Ngoc Hai <haingocvu@hotmail.com>
2019-09-29 22:30:30 +0700

    styling buildControls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import classes from './Burger.module.scss';
 import BurgerIncredient from './BurgerIngredient/BurgerIncredient';
+import Proptypes from 'prop-types';
 
 const Burger = (props) => {
+
+    const showIngredients = () => {
+        const { ingredients } = props;
+        let ingredientsArr = Object.keys(ingredients).map(igKey => {
+            return [...Array(ingredients[igKey])].map((_, i) =>
+                <BurgerIncredient key={igKey + i} type={igKey} />
+            )
+        }).reduce((accu, curr) => [...accu, ...curr], []);
+        ingredientsArr = ingredientsArr.length ? ingredientsArr :
+            <p>Please start add ingredients</p>
+        return ingredientsArr;
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIncredient type="bread-top" />
-            <BurgerIncredient type="cheese" />
-            <BurgerIncredient type="meat" />
+            {showIngredients()}
             <BurgerIncredient type="bread-bottom" />
         </div>
     )
+}
+
+Burger.prototype = {
+    ingredients: Proptypes.object
 }
 
 export default Burger;
--- src/containers/BurgerBuilder/BergerBuilder.js
+++ src/containers/BurgerBuilder/BergerBuilder.js
@@ -3,16 +3,18 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
+    state = {
+        ingredients: {
+            meat: 0,
+            cheese: 0,
+            salad: 0,
+            bacon: 0 
         }
     }
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         )
